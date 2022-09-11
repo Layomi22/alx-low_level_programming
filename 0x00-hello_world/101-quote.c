cfa306d094611a7c6cd63b2a9be399923b65e6cd
Layomi22 <sewejlayomi18@gmail.com>
2022-09-11 06:33:34 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
-#include<stdio.h>
-#include<stdlib.h>
+l#include<unistd.h>
 
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 ((Success)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\n":Dora Korpar, 2015-10-19\n",59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
