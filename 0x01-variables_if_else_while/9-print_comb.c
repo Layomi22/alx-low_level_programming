873ba2291f87d32536cfc830afefa72b0c184965
Layomi22 <112495209+Layomi22@users.noreply.github.com>
2022-09-13 06:13:46 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,7 +6,7 @@ int main(void)
 {
 	int d;
 
-	for (d = 0; d <= '9'; d++)
+	for (d = '0'; d <= '9'; d++)
 {
 		putchar (d);
 	if (d != '9')
@@ -15,7 +15,7 @@ int main(void)
 		putchar('');
 	}
 }
-putchar('\n')%
+putchar('\n');
 
 return (0);
 }
