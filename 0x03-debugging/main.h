29b56b17c9224369b856aff83376ef00ab219d95
Layomi22 <sewejlayomi18@gmail.com>
2022-09-16 02:14:39 -0700

    0 task

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,12 +4,5 @@
 #include<stdio.h>
 
 void positive_or_negative(int i);
-<<<<<<< HEAD
-void main(int i);
-=======
-void main(int);
->>>>>>> 87d71cda6e1c04aeefc50e4f91c7992556929746
-int largest_number(int, int, int);
-void print_remaining_days(int month, int day, int year);
 
 #endif /*MAIN_H*/
