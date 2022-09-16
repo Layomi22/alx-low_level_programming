d9a624045307e1aa67d53db336e76b58c6c4afde
Layomi22 <sewejlayomi18@gmail.com>
2022-09-16 01:58:43 -0700

    0 task

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
 
 #include<stdio.h>
 
 void positive_or_negative(int i);
-
-
+void main(int i);
+int largest_number(int, int, int);
+void print_remaining_days(int month, int day, int year);
 
 #endif /*MAIN_H*/
