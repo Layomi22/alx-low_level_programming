87d71cda6e1c04aeefc50e4f91c7992556929746
Layomi22 <112495209+Layomi22@users.noreply.github.com>
2022-09-16 09:41:11 +0100

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,5 +4,8 @@
 #include<stdio.h>
 
 void positive_or_negative(int i);
+void main(int);
+int largest_number(int, int, int);
+void print_remaining_days(int month, int day, int year);
 
 #endif /*MAIN_H*/
