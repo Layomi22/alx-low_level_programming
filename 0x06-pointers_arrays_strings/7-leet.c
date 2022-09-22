747adf8ee3166fdfe25f5c11ffadb0f3104e63a0
Layomi22 <sewejlayomi18@gmail.com>
2022-09-21 17:16:56 -0700

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,25 +1,38 @@
 #include "main.h"
 
 /**
- * leet - encodes a string into 1337
- * @s: string to encode
+ * *leet - check the code for Holberton School students.
  *
- * Return: address of s
+ * @str: string.
+ * Return: string.
  */
-char *leet(char *s)
+char *leet(char *str)
 {
-	int i, j;
-	char a[] = "aAeEoOtTlL";
-	char b[] = "4433007711";
+	int i;
 
-	for (i = 0; *(s + i); i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		if (str[i] == 'a' || str[i] == 'A')
 		{
-			if (a[j] == *(s + i))
-				*(s + i) = b[j];
+			str[i] = '4';
+		}
+		else if (str[i] == 'e' || str[i] == 'E')
+		{
+			str[i] = '3';
+		}
+		else if (str[i] == 'o' || str[i] == 'O')
+		{
+			str[i] = '0';
+		}
+		else if (str[i] == 't' || str[i] == 'T')
+		{
+			str[i] = '7';
+		}
+		else if (str[i] == 'l' || str[i] == 'L')
+		{
+			str[i] = '1';
 		}
 	}
-	return (s);
+	return (str);
 }
 
